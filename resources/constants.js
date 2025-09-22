a0aa0d4e0ad741a10981935b47f0f88ee2cf6773
datdo178 <datdo178@gmail.com>
2025-09-22 09:09:10 +0700

    Add common variables to contants file

---
+++ resources/constants.js
@@ -0,0 +1,69 @@
+import path from "path";
+
+export const SUPPORTED_TYPES = [
+  "composer",
+  "go",
+  "maven",
+  "npm",
+  "nuget",
+  "pypi",
+  "rubygems",
+];
+
+export const EXCLUDED_URL_PATHS = [
+  "com.cybozu.",
+  "com.cybersource.",
+  "com.gmo_pg.g_pay.client",
+  "@kintone-plugins",
+];
+
+export const CSV_DIR = {
+  output: path.join(process.cwd(), "output"),
+  manualSplit: path.join(
+    process.cwd(),
+    "output",
+    "manual_splitBy_teamRepository",
+  ),
+  eolSplit: path.join(process.cwd(), "output", "eol_splitBy_teamRepository"),
+  errorSplit: path.join(
+    process.cwd(),
+    "output",
+    "error_splitBy_teamRepository",
+  ),
+};
+
+export const CSV_PATH = {
+  manual: path.join(CSV_DIR.output, "manual.csv"),
+  eol: path.join(CSV_DIR.output, "eol.csv"),
+  error: path.join(CSV_DIR.output, "error.csv"),
+};
+
+export const YAMORY_HEADER_JP = {
+  "license risk": "ライセンスリスク",
+  team: "チーム",
+  "repository/project group": "リポジトリ/プロジェクトグループ",
+  "manifest/project": "マニフェスト/プロジェクト",
+  "package type": "パッケージタイプ",
+  software: "ソフトウェア",
+  license: "ライセンス",
+  "includes license without OSI certification":
+    "OSI 認定なしのライセンスを含む",
+  "project site": "プロジェクトサイト",
+  "source code repository": "ソースコードリポジトリ",
+  "package repository": "パッケージリポジトリ",
+};
+
+export const EOL_HEADER_JP = {
+  "last publish version": "最終公開バージョン",
+  "last publish date": "最終公開日",
+  "different in month": "経過月数",
+  ...YAMORY_HEADER_JP,
+};
+
+export const YAMORY_HEADER_EN = Object.fromEntries(
+  Object.entries(YAMORY_HEADER_JP).map(([key, value]) => [value, key]),
+);
+
+export const EOL_HEADER_EN = Object.fromEntries(
+  Object.entries(EOL_HEADER_JP).map(([key, value]) => [value, key]),
+);
